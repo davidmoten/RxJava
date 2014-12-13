7f8534ada57591798d2d42f172239a409b7d0d35
Dave Moten <davidmoten@gmail.com>
2014-12-13 20:38:35 +1100

    OperatorMerge2 dev

--- src/main/java/rx/internal/operators/OperatorMerge2.java
+++ src/main/java/rx/internal/operators/OperatorMerge2.java
@@ -10,13 +10,13 @@ import rx.Observable.Operator;
 import rx.Producer;
 import rx.Subscriber;
 
-public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
+public class OperatorMerge2<T> implements Operator<T, Observable<? extends T>> {
 
     @Override
-    public Subscriber<? super Observable<T>> call(Subscriber<? super T> child) {
+    public Subscriber<? super Observable<? extends T>> call(Subscriber<? super T> child) {
         Coordinator<T> coordinator = new Coordinator<T>(child);
         child.setProducer(new MergeProducer<T>(coordinator));
-        Subscriber<Observable<T>> parent = new MergeSubscriber<T>(coordinator);
+        Subscriber<Observable<? extends T>> parent = new MergeSubscriber<T>(coordinator);
         // if child unsubscribes it should unsubscribe the parent, but not the
         // other way around
         child.add(parent);
@@ -31,7 +31,7 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
         private final List<InnerSubscriber<T>> innerSubscribers = new CopyOnWriteArrayList<InnerSubscriber<T>>();;
         private final AtomicLong expecting = new AtomicLong();
         private volatile boolean requestedAll = false;
-        private volatile boolean addingInners = true;
+        private volatile boolean innersComplete = true;
         final ConcurrentLinkedQueue<Object> queue = new ConcurrentLinkedQueue<Object>();
 
         public Coordinator(Subscriber<? super T> child) {
@@ -42,7 +42,7 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
             child.onError(e);
         }
 
-        public void add(Observable<T> inner) {
+        public void add(Observable<? extends T> inner) {
             InnerSubscriber<T> sub = new InnerSubscriber<T>(this);
             innerSubscribers.add(sub);
             inner.subscribe(sub);
@@ -52,7 +52,7 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
             queue.add(on.next(t));
             drainQueue();
             if (expecting.get() > 0 && innerExpecting == 0) {
-                inner.requestMore(expecting.get());
+                inner.requestMore(InnerSubscriber.DEFAULT_INNER_REQUEST);
             }
         }
 
@@ -86,12 +86,12 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
         }
 
         public void onCompleted() {
-            addingInners = false;
+            innersComplete = false;
         }
 
         public void onCompleted(InnerSubscriber<T> inner) {
             innerSubscribers.remove(inner);
-            if (innerSubscribers.size() == 0 && !addingInners) {
+            if (innerSubscribers.size() == 0 && !innersComplete) {
                 queue.add(on.completed());
             }
         }
@@ -113,7 +113,7 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
 
     }
 
-    private static class MergeSubscriber<T> extends Subscriber<Observable<T>> {
+    private static class MergeSubscriber<T> extends Subscriber<Observable<? extends T>> {
 
         private Coordinator<T> coordinator;
 
@@ -132,7 +132,7 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
         }
 
         @Override
-        public void onNext(Observable<T> inner) {
+        public void onNext(Observable<? extends T> inner) {
             coordinator.add(inner);
         }
 
@@ -140,7 +140,7 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
 
     private static class InnerSubscriber<T> extends Subscriber<T> {
 
-        private static final int MAX_REQUESTS = 128;
+        static final int DEFAULT_INNER_REQUEST = 128;
         private final AtomicLong expecting = new AtomicLong(0);
         private final Coordinator<T> coordinator;
 
@@ -149,11 +149,9 @@ public class OperatorMerge2<T> implements Operator<T, Observable<T>> {
         }
 
         public synchronized void requestMore(long n) {
-            long more = Math.min(MAX_REQUESTS - expecting.get(), n);
-            if (more > 0) {
-                expecting.addAndGet(more);
-                request(more);
-            }
+            long more = Math.min(DEFAULT_INNER_REQUEST - expecting.get(), n);
+            expecting.addAndGet(more);
+            request(more);
         }
 
         @Override
