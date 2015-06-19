6697f220baa91a79227099dd3908a759d6bda8f1
Dave Moten <davidmoten@gmail.com>
2015-06-19 21:17:11 +1000

    add perfs

---
+++ src/perf/java/rx/operators/EmptyOptimizationPerf.java
@@ -0,0 +1,31 @@
+package rx.operators;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+import rx.Observable;
+
+public class EmptyOptimizationPerf {
+    
+    /*
+Benchmark                                        Mode   Samples        Score  Score error    Units
+r.o.EmptyOptimizationPerf.testMergeWithEmpty    thrpt         5  1044473.792   141571.867    ops/s
+r.o.EmptyOptimizationPerf.testNoMerge           thrpt         5  9946250.934   874336.704    ops/s
+     */
+    
+    /*
+./gradlew benchmarks "-Pjmh=-f 1 -tu s -bm thrpt -wi 5 -i 5 -r 1 .*EmptyOptimizationPerf.*"
+     */
+
+    private static final Observable<Integer> o = Observable.just(1, 2, 3, 4, 5);
+    
+    @Benchmark
+    public void testMergeWithEmpty() {
+        o.mergeWith(Observable.<Integer>empty()).subscribe();
+    }
+    
+    @Benchmark
+    public void testNoMerge() {
+        o.subscribe();
+    }
+
+}
